4ca221b89b9f791732ad34f04d227e9fdd6b8222
palmer-andrew <andrewpalmerschool@gmail.com>
2023-04-23 00:52:05 -0500

    week 1 prep 1 and 2

--- csharp-prep/Prep2/Program.cs
+++ csharp-prep/Prep2/Program.cs
@@ -4,6 +4,37 @@ class Program
 {
     static void Main(string[] args)
     {
-        Console.WriteLine("Hello Prep2 World!");
+        Console.Write("What is your grade percentage?");
+        string grade = Console.ReadLine();
+        int gradeNumber = int.Parse(grade);
+
+        if (gradeNumber >= 90)
+        {
+            string gradeLetter = "A";
+            Console.Write($"your grade is {gradeLetter}. You passed!");
+        }
+        
+        else if (gradeNumber >= 80 && gradeNumber <90)
+        {
+            string gradeLetter = "B";
+            Console.Write($"your grade is {gradeLetter}. You passed!");
+        }
+        else if (gradeNumber >= 70 && gradeNumber <80)
+        {
+            string gradeLetter = "C";
+            Console.Write($"your grade is {gradeLetter}. You passed!");
+        }
+        else if (gradeNumber >= 60 && gradeNumber <70)
+        {
+            string gradeLetter = "D";
+            Console.Write($"your grade is {gradeLetter}. You did not pass.");
+        }
+        else if (gradeNumber <60)
+        {
+            string gradeLetter = "F";
+            Console.Write($"your grade is {gradeLetter}. You did not pass.");
+        }
+        
+
     }
 }
