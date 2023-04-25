95ecd24d6bb6c4418a9968ef64470576323839e8
palmer-andrew <andrewpalmerschool@gmail.com>
2023-04-24 21:40:15 -0500

    prep3 assignment

--- csharp-prep/Prep3/Program.cs
+++ csharp-prep/Prep3/Program.cs
@@ -4,6 +4,29 @@ class Program
 {
     static void Main(string[] args)
     {
-        Console.WriteLine("Hello Prep3 World!");
+
+        Random randomGenerator = new Random();
+        int magicNumber = randomGenerator.Next(1, 100);
+
+        int guess = 101;
+
+        do
+        {
+            Console.Write("What is the your guess? ");
+            guess = int.Parse(Console.ReadLine());
+
+            if (guess < magicNumber)
+            {
+                Console.WriteLine("Higher");
+            }
+            else if (guess > magicNumber )
+            {
+                Console.WriteLine("Lower");
+            }
+            else if (guess == magicNumber)
+            {
+                Console.WriteLine("You guessed it!");
+            }
+        } while (guess != magicNumber);
     }
 }
