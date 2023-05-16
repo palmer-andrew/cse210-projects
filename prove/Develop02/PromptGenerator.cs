5dbb4485a95e513fb9abe2f3109f3e4594399498
palmer-andrew <andrewpalmerschool@gmail.com>
2023-05-15 23:59:31 -0500

    current changes

--- prove/Develop02/PromptGenerator.cs
+++ prove/Develop02/PromptGenerator.cs
@@ -5,12 +5,20 @@ public class PromptGenerator
 {
     {   
         // creates member variable
-        public string _prompts(void);
+        public string _prompts;
+
+        // Creates a new instance of Prompt
+        Prompt _prompt1 = new Prompt();
+        prompt1 = "Who did you talk to today?";
+        prompt2 = "What scripture did you read today?";
+        prompt3 = "What spiritual promptings did you receive today?";
+        prompt4 = "What was the most inspiring thing you saw or heard today?";
+        prompt5 = "What are you hoping to happen tomorrow?";
 
         // Function that generates a prompt when given code 'Prompt();'
-        public(void) PromptGenerator()
+        public string PromptGenerator(void)
         {
-            
+            Console.WriteLine($"_prompt1");
         }
     }
 }
